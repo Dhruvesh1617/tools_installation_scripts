71818507b9f417404715a86d483d3a343596c089
Dhruvesh <76275019+Dhruvesh1617@users.noreply.github.com>
2025-01-16 23:29:04 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -18,8 +18,8 @@ sudo apt update -y
 sudo apt upgrade -y 
 
 sudo apt install openjdk-17-jre -y
-
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+<!-- run echo and curl command by removing \ and keeping whole command in one line-->
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \       
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
